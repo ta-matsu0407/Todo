c11b8b6b2f62d1cf5ff24468dd97b0f746d658c1
ta-matsu0407 <uu.pyonpyon@gmail.com>
2024-11-22 06:07:08 +0900

    マルチログイン修正_02

--- routes/adminAuth.php
+++ routes/adminAuth.php
@@ -11,7 +11,7 @@ use App\Http\Controllers\Admin\Auth\RegisteredUserController;
 use App\Http\Controllers\Admin\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('guest')->group(function () {
+Route::middleware('guest:admins')->group(function () {
     Route::get('register', [RegisteredUserController::class, 'create'])
         ->name('register');
 
