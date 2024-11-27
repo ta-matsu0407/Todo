9f0d8382dc101cbcf9da0a4ea8a8bb4899e66088
ta-matsu0407 <uu.pyonpyon@gmail.com>
2024-11-27 16:24:22 +0900

    ダミーデータ（factory)

--- routes/admin.php
+++ routes/admin.php
@@ -6,14 +6,14 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Admin/Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
+// Route::get('/', function () {
+//     return Inertia::render('Admin/Welcome', [
+//         'canLogin' => Route::has('login'),
+//         'canRegister' => Route::has('register'),
+//         'laravelVersion' => Application::VERSION,
+//         'phpVersion' => PHP_VERSION,
+//     ]);
+// });
 
 Route::resource('users', UsersController::class)
 ->middleware('auth:admins', 'verified');
