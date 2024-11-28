58244ec9a229f6a94d21a7afbc1bdbfc5b737189
ta-matsu0407 <uu.pyonpyon@gmail.com>
2024-11-28 22:20:27 +0900

    郵便番号か uya@mら住所登録

--- resources/js/Pages/Admin/Users/Create.vue
+++ resources/js/Pages/Admin/Users/Create.vue
@@ -4,6 +4,8 @@ import { Head } from '@inertiajs/vue3';
 import { reactive } from 'vue'
 import { router as Inertia } from '@inertiajs/core';
 import BreezeValidationErrors from '@/Components/ValidationErrors.vue';
+import { Core as YubinBangoCore } from "yubinbango-core2";
+//YubinBangoCoreのライブラリをインポート
 
 defineProps({
     errors: Object
@@ -21,6 +23,13 @@ const form = reactive({
     password: null,
     memo: null,
 })
+
+// 数字を文字に変換 第１引数が郵便番号、第２がコールバックで引数に住所
+const fetchAddress = () => {
+    new YubinBangoCore(String(form.postcode), (value) => {
+    form.address = value.region + value.locality + value.street
+    })
+}
 
 const storeUser = () => {
 Inertia.post('/admin/users', form)
@@ -85,7 +94,8 @@ Inertia.post('/admin/users', form)
                                             <div class="p-2 w-full">
                                                 <div class="relative">
                                                     <label for="postcode" class="leading-7 text-sm text-gray-600">郵便番号</label>
-                                                    <input type="number" id="postcode" name="postcode" v-model="form.postcode" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                                    <input type="text" id="postcode" name="postcode" @change="fetchAddress" v-model="form.postcode" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                                    <!-- inputの入力が終わるとchangeイベントが呼ばれる -->
                                                     <div v-if="errors.postcode">{{ errors.postcode }}</div>
                                                 </div>
                                             </div>
