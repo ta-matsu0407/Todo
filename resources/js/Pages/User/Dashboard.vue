90961c00cac101a76a2f68dbd28c8a12e3dcebc9
ta-matsu0407 <uu.pyonpyon@gmail.com>
2024-12-07 07:35:20 +0900

    ユーザー編集、更新追加

---
+++ resources/js/Pages/User/Dashboard.vue
@@ -0,0 +1,59 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head, Link } from '@inertiajs/vue3';
+
+const props = defineProps({
+  user: Object
+})
+</script>
+
+<template>
+    <Head title="Dashboard" />
+
+    <AuthenticatedLayout>
+        <template #header>
+            <h2 class="text-xl font-semibold leading-tight text-gray-800">
+                {{ user.name }}のやることリスト
+            </h2>
+        </template>
+
+        <div class="py-12">
+            <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+                <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
+                    <div class="p-6 text-gray-900">
+                        <section class="text-gray-600 body-font relative">
+                                <div class="container px-5 py-8 mx-auto">
+                                    <FlashMessage />
+                                    <div class="lg:w-1/2 md:w-2/3 mx-auto">
+                                        <div class="flex flex-wrap -m-2">
+                                            <div class="p-2 w-full">
+                                                <div class="relative">
+                                                    <label for="memo" class="leading-7 text-sm text-gray-600">メモ</label>
+                                                    <div id="memo" style="white-space: pre-wrap;" class="w-full bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out">
+                                                        <!-- style="white-space: pre-wrap;":改行がそのまま表示される -->
+                                                        {{ user.memo }}
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="p-2 w-full">
+                                                <div class="relative">
+                                                    <label for="status" class="leading-7 text-sm text-gray-600">ステータス</label>
+                                                    <div id="status" class="w-full bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                                        <span v-if="user.status === 1">実施中</span>
+                                                        <span v-if="user.status === 0">完了</span>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="p-2  w-full">
+                                                <Link as="button" :href="route('user.edit', { user: user.id})" class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">編集する</Link>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                        </section>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </AuthenticatedLayout>
+</template>
---
+++ resources/js/Pages/User/Edit.vue
@@ -0,0 +1,80 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head, Link } from '@inertiajs/vue3';
+import { reactive } from 'vue'
+import { router as Inertia } from '@inertiajs/core';
+import BreezeValidationErrors from '@/Components/ValidationErrors.vue';
+
+const props = defineProps({
+    user: Object,
+    errors: Object
+})
+
+const form = reactive({
+    id: props.user.id,
+    memo: props.user.memo,
+    status: props.user.status,
+})
+
+const updateUser = id => {
+Inertia.put(route('user.update', { user: id}), form)
+}
+//route:listをみると、updateはPUTとある
+
+</script>
+
+<template>
+    <Head title="ユーザー編集" />
+
+    <AuthenticatedLayout>
+        <template #header>
+            <h2 class="text-xl font-semibold leading-tight text-gray-800">
+                ユーザー編集
+            </h2>
+        </template>
+
+        <div class="py-12">
+            <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+                <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
+                    <div class="p-6 text-gray-900">
+                        <section class="text-gray-600 body-font relative">
+                            <form @submit.prevent="updateUser(form.id)">
+                                <!-- 更新ボタンを押したらsubmitが走り、formが走り、updateUserメソッドが走る -->
+                                <!-- sectionタグを目印にformタグ -->
+                                <!-- submit.prevent：SPA、post通信を行った時にページの読み込みを防ぐ -->
+                                <!-- updateUser：scriptで設定したメソッドを実行 -->
+                                <div class="container px-5 py-8 mx-auto">
+                                    <div class="lg:w-1/2 md:w-2/3 mx-auto">
+                                        <div class="flex flex-wrap -m-2">
+                                            <div class="p-2 w-full">
+                                                <div class="relative">
+                                                    <label for="status" class="leading-7 text-sm text-gray-600">ステータス</label>
+                                                    <input type="radio" id="status" name="status" v-model="form.status" value="1" >
+                                                    <!-- v-model="form.status"として、form オブジェクトの status プロパティにバインド -->
+                                                    <!-- value="1":statusが1の時にチェックが入る -->
+                                                    <label class="ml-2 mr-10">実施中</label>
+                                                    <input type="radio" id="status" name="status" v-model="form.status" value="0" >
+                                                    <label class="ml-2 mr-10">完了</label>
+                                                </div>
+                                            </div>
+                                            <div class="p-2 w-full">
+                                                <!-- w-full:横幅いっぱい -->
+                                                <div class="relative">
+                                                    <label for="memo" class="leading-7 text-sm text-gray-600">メモ</label>
+                                                    <textarea id="memo" name="memo" v-model="form.memo" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
+                                                </div>
+                                            </div>
+                                            <div class="p-2 w-full">
+                                                <button class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">更新</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                        </section>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </AuthenticatedLayout>
+</template>
