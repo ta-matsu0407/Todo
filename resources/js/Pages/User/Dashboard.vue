ff36aa7710ed6fa1e509322b0bcd872faff52b99
ta-matsu0407 <uu.pyonpyon@gmail.com>
2024-12-11 06:45:04 +0900

    ユーザー画面のデザイン調整

--- resources/js/Pages/User/Dashboard.vue
+++ resources/js/Pages/User/Dashboard.vue
@@ -1,6 +1,7 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head, Link } from '@inertiajs/vue3';
+import FlashMessage from '@/Components/FlashMessage.vue';
 
 const props = defineProps({
   user: Object
@@ -12,12 +13,12 @@ const props = defineProps({
 
     <AuthenticatedLayout>
         <template #header>
-            <h2 class="text-xl font-semibold leading-tight text-gray-800">
-                {{ user.name }}のやることリスト
+            <h2 class="text-2xl font-semibold leading-tight text-gray-800">
+                 {{ user.name }}のやることリスト
             </h2>
         </template>
 
-        <div class="py-12">
+        <div class="py-16">
             <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
                 <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
                     <div class="p-6 text-gray-900">
@@ -28,7 +29,8 @@ const props = defineProps({
                                         <div class="flex flex-wrap -m-2">
                                             <div class="p-2 w-full">
                                                 <div class="relative">
-                                                    <label for="memo" class="leading-7 text-sm text-gray-600">メモ</label>
+                                                    <!-- <label for="memo" class="leading-7 text-sm text-gray-600">メモ</label> -->
+                                                    <label for="memo" class="leading-7 text-lg font-semibold text-gray-800">やること</label>
                                                     <div id="memo" style="white-space: pre-wrap;" class="w-full bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out">
                                                         <!-- style="white-space: pre-wrap;":改行がそのまま表示される -->
                                                         {{ user.memo }}
@@ -37,15 +39,31 @@ const props = defineProps({
                                             </div>
                                             <div class="p-2 w-full">
                                                 <div class="relative">
-                                                    <label for="status" class="leading-7 text-sm text-gray-600">ステータス</label>
+                                                    <label for="status" class="leading-7 text-lg font-semibold text-gray-800">状況</label>
                                                     <div id="status" class="w-full bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
-                                                        <span v-if="user.status === 1">実施中</span>
-                                                        <span v-if="user.status === 0">完了</span>
+                                                        <!-- <span v-if="user.status === 1">実施中</span> -->
+                                                        <!-- <p v-if="user.status === 1" class="flex items-center text-green-500"> -->
+                                                        <p v-if="user.status === 1" class="flex items-center text-green-700 bg-green-100 rounded-lg p-2">
+                                                            <!-- <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4" />
+                                                            </svg> -->
+                                                            実施中
+                                                        </p>
+                                                        <!-- <span v-if="user.status === 0">完了</span> -->
+                                                        <!-- <p v-if="user.status === 0" class="flex items-center text-red-500"> -->
+                                                        <p v-if="user.status === 0" class="flex items-center text-red-700 bg-red-100 rounded-lg p-2">
+                                                            <!-- <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                                                            </svg> -->
+                                                            完了
+                                                        </p>
                                                     </div>
                                                 </div>
                                             </div>
                                             <div class="p-2  w-full">
-                                                <Link as="button" :href="route('user.edit', { user: user.id})" class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">編集する</Link>
+                                                <Link as="button" :href="route('user.edit', { user: user.id})" class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+                                                    編集する
+                                                </Link>
                                             </div>
                                         </div>
                                     </div>
--- resources/js/Pages/User/Edit.vue
+++ resources/js/Pages/User/Edit.vue
@@ -28,8 +28,8 @@ Inertia.put(route('user.update', { user: id}), form)
 
     <AuthenticatedLayout>
         <template #header>
-            <h2 class="text-xl font-semibold leading-tight text-gray-800">
-                ユーザー編集
+            <h2 class="text-2xl font-semibold leading-tight text-gray-800">
+                {{ user.name }}の編集画面
             </h2>
         </template>
 
@@ -48,24 +48,37 @@ Inertia.put(route('user.update', { user: id}), form)
                                         <div class="flex flex-wrap -m-2">
                                             <div class="p-2 w-full">
                                                 <div class="relative">
-                                                    <label for="status" class="leading-7 text-sm text-gray-600">ステータス</label>
-                                                    <input type="radio" id="status" name="status" v-model="form.status" value="1" >
+                                                    <label for="memo" class="leading-7 text-lg font-semibold text-gray-800">やること</label>
+                                                    <div id="memo" style="white-space: pre-wrap;" class="w-full bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out">
+                                                        {{ user.memo }}
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="p-2 w-full">
+                                                <div class="relative">
+                                                    <label for="status" class="leading-7 text-lg font-semibold text-gray-800">状況</label>
+                                                    <!-- <input type="radio" id="status" name="status" v-model="form.status" value="1" > -->
                                                     <!-- v-model="form.status"として、form オブジェクトの status プロパティにバインド -->
                                                     <!-- value="1":statusが1の時にチェックが入る -->
-                                                    <label class="ml-2 mr-10">実施中</label>
+                                                    <!-- <label class="ml-2 mr-10">実施中</label>
                                                     <input type="radio" id="status" name="status" v-model="form.status" value="0" >
-                                                    <label class="ml-2 mr-10">完了</label>
+                                                    <label class="ml-2 mr-10">完了</label> -->
+                                                    <select
+                                                        id="status"
+                                                        name="status"
+                                                        v-model="form.status"
+                                                        class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                                                    >
+                                                        <option value="1" >実施中</option>
+                                                        <option value="0">完了</option>
+                                                    </select>
                                                 </div>
                                             </div>
+
                                             <div class="p-2 w-full">
-                                                <!-- w-full:横幅いっぱい -->
-                                                <div class="relative">
-                                                    <label for="memo" class="leading-7 text-sm text-gray-600">メモ</label>
-                                                    <textarea id="memo" name="memo" v-model="form.memo" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
-                                                </div>
-                                            </div>
-                                            <div class="p-2 w-full">
-                                                <button class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">更新</button>
+                                                <button class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+                                                    更新
+                                                </button>
                                             </div>
                                         </div>
                                     </div>
